21b5491b801ea1bada922bf670222280babe84bb
S Gautam <goutamas.keladi@gmail.com>
2019-09-18 17:22:37 +0530

    Adding more get routes

--- src/routes/person.js
+++ src/routes/person.js
@@ -4,4 +4,16 @@ router.get('/person', (req, res) => {
     res.send('You have requested a person');
 })
 
+router.get('/user', (req, res) => {
+    if(req.query.name) {
+        res.send(`Hello, ${req.query.name}`)
+    } else {
+        res.send(`May i know your name?`)
+    }
+})
+
+router.get('/person/:name', (req, res) => {
+    res.send(`You have requested a person ${req.params.name}`);
+})
+
 module.exports = router;
