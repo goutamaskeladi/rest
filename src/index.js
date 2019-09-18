aaa92ec23935fd5ae947e449b29b247b609dca3e
S Gautam <goutamas.keladi@gmail.com>
2019-09-19 00:23:16 +0530

    Adding mongoose schema and model

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ const app = express();
 const path = require("path");
 const PORT = process.env.PORT || 3000;
 const personRoute = require('./routes/person');
+const customerRoute = require('./routes/customer');
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(personRoute);
+app.use(customerRoute);
 app.use((req, res, next) => {
     res.status(404).send("<h2>The page you are looking for doesn't exist!</h2>");
 })
