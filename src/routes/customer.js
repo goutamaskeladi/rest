a3a273ce6459a8607a8035010bdf91f0730f63f0
S Gautam <goutamas.keladi@gmail.com>
2019-09-19 11:28:50 +0530

    Added CRUD operations

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -14,4 +14,39 @@ router.post('/customer', (req, res) => {
      .catch( err => res.status(500).send(err));
 })
 
+router.get('/customer', (req, res) => {
+    if(!req.query.email) {
+       return res.status(400).send("Email is missing!");
+    }
+    CustomerModel.findOne({
+        email : req.query.email
+    })
+    .then(doc => res.json(doc))
+    .catch(err => res.status(500).json(err));
+})
+
+router.put('/customer', (req, res) => {
+    if(!req.query.email) {
+        return res.status(400).send("Email is missing!");
+    }
+    CustomerModel.findOneAndUpdate({
+        email : req.query.email
+    }, {
+        name : req.query.name
+    })
+    .then( doc => res.json(doc))
+    .catch( err => res.status(500).send(err))
+})
+
+router.delete('/customer', (req, res) => {
+    if(!req.query.email) {
+        return res.status(400).send("Email is missing!");
+    }
+    CustomerModel.findOneAndDelete({
+        email : req.query.email
+    })
+    .then( doc => res.json(doc))
+    .catch( err => res.status(500).send(err))
+})
+
 module.exports = router;
